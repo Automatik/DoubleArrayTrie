c0bdec34c40ae175fda196ad213141409a530be0
e.osterhed <e.osterhed@campus.unimib.it>
2019-11-27 21:36:32 +0100

    Correct xCheck method

--- src/main/trie/DoubleArrayTrie.java
+++ src/main/trie/DoubleArrayTrie.java
@@ -378,9 +378,12 @@ public class DoubleArrayTrie implements Serializable {
         }
 
         //If there are no gaps
-        int neededPositions = maxOffset - minOffset + 1;
-        ensureReachableIndex(base.size() + neededPositions - 1);
-        int q = base.size() - neededPositions - minOffset;
+//        int neededPositions = maxOffset - minOffset + 1;
+//        ensureReachableIndex(base.size() + neededPositions - 1);
+//        int q = base.size() - neededPositions - minOffset;
+//        assert q > 0;
+        int q = base.size() - 1;
+        ensureReachableIndex(q + maxOffset);
         assert q > 0;
         return q;
     }
